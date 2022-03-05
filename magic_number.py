d472438fc0328fc8d7a6f7179fe8f87a9ed85b1b
Robert Vári <mail.robertvari@gmail.com>
2022-03-05 16:59:20 +0100

    update

---
+++ magic_number.py
@@ -0,0 +1,23 @@
+min_number = 0
+max_number = 10
+max_tries = 3
+
+
+def main():
+    # todo print some intro about the game
+
+    # todo get a random number: import random, random.randint(a, b)
+
+    # todo get player number
+
+    # todo while check player number
+        # todo repeat till player gets the magic_number
+        # todo or player runs out of max_tries
+
+    # todo print game result
+
+    # todo ask player if he/she wants to play again
+    pass
+
+
+main()
