4726902a78d4e2c1e7a50140aaa88c3a4d4545c9
Robert Vári <mail.robertvari@gmail.com>
2022-03-05 14:35:03 +0100

    update

---
+++ 03_multiple_posittional_parameters.py
@@ -0,0 +1,7 @@
+def say_hello(name, age, email):
+    print(f"Hello {name}.")
+    print(f"You are {age} years old.")
+    print(f"Your email is {email}.")
+
+
+say_hello("Robert", 32, "robert@gmail.com")
