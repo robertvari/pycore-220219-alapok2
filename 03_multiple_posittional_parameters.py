749ad2f0c83b5e03a5d83cd6f4bf99f40f557531
Robert Vári <mail.robertvari@gmail.com>
2022-03-05 14:40:55 +0100

    update

--- 03_multiple_posittional_parameters.py
+++ 03_multiple_posittional_parameters.py
@@ -1,7 +1,14 @@
-def say_hello(name, age, email):
+def say_hello(address, name, age, email):
     print(f"Hello {name}.")
     print(f"You are {age} years old.")
     print(f"Your email is {email}.")
+    print(f"Your address {address}.")
 
 
-say_hello("Robert", 32, "robert@gmail.com")
+say_hello("Budapest", "Robert", 32, "robert@gmail.com")
+say_hello(
+    email="tamas@gmail.com",
+    age=23,
+    name="Tamás",
+    address="Budapest",
+)
