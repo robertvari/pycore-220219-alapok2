77a5f70405565bb10dc62f395961d473042e653d
Robert Vári <mail.robertvari@gmail.com>
2022-03-05 15:08:17 +0100

    update

---
+++ 06_docstrings.py
@@ -0,0 +1,13 @@
+def say_hello(name: str, age: int):
+    """
+    This is my first python function.
+    :param name: Your name (str)
+    :param age: Your age (int)
+    :return:
+    """
+
+    print(f"Hello {name}.")
+    print(f"You are {age} years old.")
+
+
+say_hello("Robert", 32)
