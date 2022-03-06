9adb459a5e6c5d3290c370b1d05478953eab2468
Robert Vári <mail.robertvari@gmail.com>
2022-03-06 15:56:44 +0100

    update

---
+++ 15_threading.py
@@ -0,0 +1,13 @@
+import random, time
+
+
+def worker1():
+    print("Worker1 started")
+    time.sleep(random.randint(1, 10))
+    print("Worker1 finished")
+
+
+def worker2():
+    print("Worker2 started")
+    time.sleep(random.randint(1, 10))
+    print("Worker2 finished")
