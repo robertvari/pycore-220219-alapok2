1a4b95d83ac2cfbb3d1f370c23ab2c2590500f93
Robert Vári <mail.robertvari@gmail.com>
2022-03-06 14:44:22 +0100

    update

--- 12_recursion.py
+++ 12_recursion.py
@@ -1,9 +1,18 @@
+from utilities.file_utils import get_files
+
+
 def get_factorial(n):
     if n < 2:
         return n
     else:
         return n * get_factorial(n-1)
 
+# result = get_factorial(5)
+# print(result)
 
-result = get_factorial(5)
-print(result)
+file_list = []
+get_files(
+    root_folder=r"C:\Work\_PythonSuli\pycore-220219\photos",
+    files=file_list,
+    name_filet=".xlsx"
+)
--- utilities/file_utils.py
+++ utilities/file_utils.py
@@ -1,21 +1,34 @@
 import os
 
 
-def get_files(root_folder: str, name_filet=None) -> list:
+def get_files(root_folder: str, files: list, name_filet=None) -> list:
     """
     This functions takes a string as a folder path and returns a list of files (str)
     :param root_folder: path like string
     :param name_filet: Optional filter on files
+    :param files: Empty list for storing files
     :return: List of files list[str]
     """
 
     assert os.path.exists(root_folder), f"Folder: {root_folder} does not exist."
     assert os.path.isdir(root_folder), f"root_folder must be a folder."
 
-    # list comprehension
-    result = [os.path.join(root_folder, i) for i in os.listdir(root_folder)]
+    folder_content = [os.path.join(root_folder, i) for i in os.listdir(root_folder)]
 
-    if name_filet:
-        result = [i for i in result if name_filet.lower() in i.lower()]
+    # get all files in current root_folder
+    for i in folder_content:
+        if os.path.isfile(i):
+            if name_filet:
+                if name_filet.lower() in i.lower():
+                    files.append(i)
+            else:
+                files.append(i)
 
-    return result
+    # find all folders in folder_content
+    subfolders = []
+    for i in folder_content:
+        if os.path.isdir(i):
+            subfolders.append(i)
+
+    for folder in subfolders:
+        get_files(folder, files)
