74f219aba1b4c0a955088db1cdd2cfa7f5c2e2b3
Robert Vári <mail.robertvari@gmail.com>
2022-03-06 17:10:37 +0100

    update

--- 18_threading_with_parameters.py
+++ 18_threading_with_parameters.py
@@ -1,4 +1,13 @@
+from utilities.file_utils import get_files
 import random, time, threading
+
+# get all photos from root_folder
+root_folder = r"C:\Work\_PythonSuli\pycore-220219\photos"
+photo_list = []
+get_files(
+    root_folder=root_folder,
+    files=photo_list
+)
 
 
 def worker1(file_list):
@@ -6,3 +15,7 @@ def worker1(file_list):
         print(f"Worker1 started on {i}")
         time.sleep(random.randint(1, 4))
         print(f"Worker1 finished on {i}")
+
+
+t1 = threading.Thread(target=worker1, args=[photo_list])
+t1.start()
