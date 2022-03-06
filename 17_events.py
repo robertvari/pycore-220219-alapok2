7db3d2f4074874a6544ef66e10d7a81e1ad52e4d
Robert Vári <mail.robertvari@gmail.com>
2022-03-06 17:03:47 +0100

    update

--- 17_events.py
+++ 17_events.py
@@ -1,13 +1,28 @@
 import time, threading
 
+download_finished = threading.Event()
+
 
 def downloader_worker():
+    download_finished.clear()
+
     print("Download Started...")
     time.sleep(5)
     print("Download Finished!")
 
+    download_finished.set()
+
 
 def file_scanner_worker():
+    download_finished.wait()
+
     print("File Scanner Started")
     time.sleep(4)
     print("File Scanner Finished")
+
+
+t1 = threading.Thread(target=downloader_worker)
+t2 = threading.Thread(target=file_scanner_worker)
+
+t1.start()
+t2.start()
