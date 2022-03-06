b5c20afc3faf8f62d80de1f7286d716c76f2835c
Robert Vári <mail.robertvari@gmail.com>
2022-03-06 15:27:48 +0100

    update

--- 13_decorators.py
+++ 13_decorators.py
@@ -1,22 +1,29 @@
 import random, time
+from utilities.decorators import timer, useless_decorator
 
 
-def worker1():
+@timer
+def worker1(sleep_time):
     print("Worker1 started")
-    time.sleep(random.randint(1, 10))
+    time.sleep(sleep_time)
     print("Worker1 finished")
 
+    return sleep_time
 
+
+@timer
 def worker2():
     print("Worker2 started")
     time.sleep(random.randint(1, 10))
     print("Worker2 finished")
 
 
+@timer
 def worker3():
     print("Worker3 started")
     time.sleep(random.randint(1, 10))
     print("Worker3 finished")
 
 
-print(worker1)
+result = worker1(3)
+print(result)
--- utilities/decorators.py
+++ utilities/decorators.py
@@ -1,4 +1,21 @@
+import time, json
+
+
 def timer(function):
-    def wrapper():
-        function()
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = function(*args, **kwargs)
+        print(f"Process time: {time.time()-start_time}")
+
+        with open("timer_data.txt", "a") as f:
+            f.write(f"\nProcess time: {time.time()-start_time}")
+
+        return result
+    return wrapper
+
+
+def useless_decorator(function):
+    def wrapper(*args, **kwargs):
+        print("Hello. I'm the useless_decorator")
+        function(*args, **kwargs)
     return wrapper
