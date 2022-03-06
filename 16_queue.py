84075cb6a13637cf0a487fd5662fe8122a6b3dbe
Robert Vári <mail.robertvari@gmail.com>
2022-03-06 16:46:53 +0100

    update

--- 16_queue.py
+++ 16_queue.py
@@ -15,3 +15,12 @@ job_queue = queue.Queue()
 # update queue with photo_list
 for file in photo_list:
     job_queue.put(file)
+
+
+def photo_worker():
+    while not job_queue.empty():
+        my_job = job_queue.get()
+        print(f"{threading.current_thread().name} I'm working on: {my_job}")
+        time.sleep(random.randint(1-20))
+        print(f"{threading.current_thread().name} I finished my job on: {my_job}")
+        job_queue.task_done()
